1b0da98b019e0bd73ecec815438e510e852af5c4
Abhilash <abhilash.137@hotmail.com>
2022-10-17 15:54:29 +0530

    Added Profiles viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path
+from django.urls import path, include
 from profiles_api import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset',views.HelloViewSet,basename='hello-viewset')
+router.register("profile",views.UserProfileViewSet)
+
+
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path("hello-view/",views.HelloApiView.as_view()),
+    path('',include(router.urls)),
 ]
